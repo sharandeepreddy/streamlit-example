77f0c93dd9b3e74167b209054939d7b001a91a48
Sharandeep reddy <119688069+sharandeepreddy@users.noreply.github.com>
2024-03-11 19:48:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,40 @@
-import streamlit as st
+from skimage.io import imread
+from skimage import color
+import matplotlib.pyplot as plt
+from skimage.filters import threshold_otsu,gaussian
+from skimage.transform import resize
+from numpy import asarray
+from skimage.metrics import structural_similarity
+from skimage import measure
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
 import joblib
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
 import numpy as np
+import os
+from natsort import natsorted
+from sklearn import linear_model, tree, ensemble
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 
-# Define the ModelLoad_predict function
-def ModelLoad_predict(final_df):
-    """
-    This Function Loads the pretrained model and performs ECG classification
-    return the classification Type.
-    """
-    # Load the pretrained model from the pickle file
-    loaded_model = joblib.load('/content/Heart_Disease_Prediction_using_ECG (4).pkl')
-    
-    # Perform prediction using the loaded model
-    result = loaded_model.predict(final_df)
-    
-    # Translate prediction to human-readable format
-    if result[0] == 1:
-        return "Your ECG corresponds to Myocardial Infarction"
-    elif result[0] == 0:
-        return "Your ECG corresponds to Abnormal Heartbeat"
-    elif result[0] == 2:
-        return "Your ECG is Normal"
-    else:
-        return "Your ECG corresponds to History of Myocardial Infarction"
+class ECG:
+	def  getImage(self,image):
+		"""
+		this functions gets user image
+		return: user image
+		"""
+		image=imread(image)
+		return image
 
-# Streamlit app
-def main():
-    st.title('ECG Classification App')
-
-    # Input interface for ECG data
-    ecg_data = st.text_area('Enter ECG data (comma-separated)', '')
-
-    # Convert input data to numpy array
-    if ecg_data:
-        final_df = np.array([float(x) for x in ecg_data.split(',')]).reshape(1, -1)
-    else:
-        final_df = None
-
-    # Call the ModelLoad_predict function to get the prediction
-    if final_df is not None:
-        prediction = ModelLoad_predict(final_df)
-        st.write('Prediction:', prediction)
-
-if __name__ == '__main__':
-    main()
+	def ModelLoad_predict(self,final_df):
+		loaded_model = joblib.load('Heart_Disease_Prediction_using_ECG (4).pkl')
+		result = loaded_model.predict(final_df)
+		if result[0] == 1:
+			return "You ECG corresponds to Myocardial Infarction"
+		elif result[0] == 0:
+			return "You ECG corresponds to Abnormal Heartbeat"
+		elif result[0] == 2:
+			return "Your ECG is Normal"
+		else:
+			return "You ECG corresponds to History of Myocardial Infarction"
