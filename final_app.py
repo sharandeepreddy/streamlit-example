a997f4d054e67d1fe330dc41dde26b565a06f5df
Sharandeep reddy <119688069+sharandeepreddy@users.noreply.github.com>
2024-03-11 20:14:14 +0530

    Update final_app.py

--- final_app.py
+++ final_app.py
@@ -1,20 +1,25 @@
 import streamlit as st
-from Ecg import  ECG
-#intialize ecg object
+from Ecg import ECG
+
+# Initialize ecg object
 ecg = ECG()
-#get the uploaded image
+
+# Get the uploaded image
 uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
-  """#### **UPLOADED IMAGE**"""
-  # call the getimage method
-  ecg_user_image_read = ecg.getImage(uploaded_file)
-  #show the image
-  st.image(ecg_user_image_read)
+    """#### **UPLOADED IMAGE**"""
+    # Call the getImage method
+    ecg_user_image_read = ecg.getImage(uploaded_file)
+    # Show the image
+    st.image(ecg_user_image_read)
 
-  """#### **PASS TO PRETRAINED ML MODEL FOR PREDICTION**"""
-  #call the Pretrainsed ML model for prediction
-  ecg_model=ecg.ModelLoad_predict(ecg_final)
-  my_expander5 = st.expander(label='PREDICTION')
-  with my_expander5:
-    st.write(ecg_model)
+    """#### **PASS TO PRETRAINED ML MODEL FOR PREDICTION**"""
+    # Assume ecg_final is a valid DataFrame containing the required features
+    # You need to prepare this DataFrame based on your application logic
+    ecg_final = prepare_ecg_final(uploaded_file)  # You need to define prepare_ecg_final function
+    # Call the Pretrained ML model for prediction
+    ecg_model = ecg.ModelLoad_predict(ecg_final)
+    my_expander5 = st.expander(label='PREDICTION')
+    with my_expander5:
+        st.write(ecg_model)
